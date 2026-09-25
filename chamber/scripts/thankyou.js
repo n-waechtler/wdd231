13ffd2bca20a98937d012f33413bfe8849c44dcf
n-waechtler <njwaechtler89@gmail.com>
2026-09-24 19:43:12 -0600

    created css, html's,, and javascipt for join page

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,23 @@
+const currentYearElement = document.getElementById("currentYear");
+if (currentYearElement) {
+    currentYearElement.textContent = new Date().getFullYear();
+}
+
+const lastModifiedElement = document.getElementById("lastModified");
+if (lastModifiedElement) {
+    lastModifiedElement.innerHTML = `Last Modification: ${document.lastModified}`;
+}
+
+const menuButton = document.getElementById("menuButton");
+const navMenu = document.getElementById("navMenu")
+
+if (menuButton && navMenu) {
+    menuButton.addEventListener('click', () => {
+        navMenu.classList.toggle("open");
+        if (navMenu.classList.contains("open")) {
+            menuButton.textContent = "x";
+        } else {
+            menuButton.textContent = "≡";
+        }
+    });
+}
